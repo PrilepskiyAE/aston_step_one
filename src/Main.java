11c0644d5b7db210d1bc40d03ce6984f9ef1943f
Prilepskiy_AE <alexey.prilepskiy1992@gmail.com>
2026-05-06 16:30:41 +0700

    Homework two

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 import homework_one.ImmutablePerson;
 import homework_one.Person;
+import homework_two.Book;
+import homework_two.Student;
+import homework_two.StudentManager;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //  homeworkOne();
+        homeworkTwo();
+    }
+
+    static void homeworkOne() {
         //Homework one
         // Реализовать свой иммутабельный класс, который будет внутри себя содержать поле с изменяемым классом.
         // для удобства я создал два класса ImmutablePerson Person
         Person originalPerson = new Person("Alex", 33);
         ImmutablePerson immutablePerson = new ImmutablePerson(originalPerson);
-        System.out.println("Исходное состояние: " + immutablePerson );
+        System.out.println("Исходное состояние: " + immutablePerson);
 
         originalPerson.setAge(34);
         originalPerson.setName("Prilepskiy Alex");
@@ -16,7 +26,7 @@ public class Main {
         System.out.println("После изменения исходного Person: " + immutablePerson);
         System.out.println("Исходный Person теперь: " + originalPerson);
 
-        Person personFromImmutable= immutablePerson.getPerson();
+        Person personFromImmutable = immutablePerson.getPerson();
         personFromImmutable.setAge(-100);
         personFromImmutable.setName("Hacker");
 
@@ -31,5 +41,24 @@ public class Main {
          После попытки изменения через геттер: ImmutablePerson{person=Person{name='Prilepskiy Alex', age=34}}
          Копия из геттера: Person{name='Hacker', age=-100}
         */
+    }
+
+    static void homeworkTwo() {
+        List<Student> students = StudentManager.readStudentsFromFile("students.txt");
+       // System.out.println("Загруженные студенты:\n");
+       // students.forEach(System.out::println);
+        System.out.println("-----------------------\n");
+
+        students
+                .stream()
+                .peek(System.out::println)
+                .flatMap(student -> student.getBooks().stream())
+                .sorted()
+                .distinct()
+                .filter(book -> book.getYear() > 2000).limit(3).map(Book::getYear).findFirst()
+                .ifPresentOrElse(
+                        year -> System.out.println("Год выпуска найденной книги: " + year),
+                        () -> System.out.println("Книга не найдена")
+                );
     }
 }
